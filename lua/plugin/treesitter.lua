084659cd4ffadc50d5e199e6b3f1081e470a9978
anoninus <anoninuxt@gmail.com>
2026-06-08 19:50:46 +0530

    Treesitters features are restored!

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -3,11 +3,11 @@ return {
         "nvim-treesitter/nvim-treesitter",
         lazy = true,
         build = ":TSUpdate",
-        event = "VeryLazy",  -- triggers after UI is fully rendered
+        event = "VeryLazy",
 
         config = function()
-            -- Defer the entire setup by 50ms after VeryLazy fires
             vim.defer_fn(function()
+                -- 1. Load & install
                 require("nvim-treesitter").setup({
                     install_dir = vim.fn.stdpath("data") .. "/site",
                 })
@@ -19,23 +19,31 @@ return {
                     })
                     :wait(300000)
 
+                -- 2. Start treesitter on current buffer
+                pcall(vim.treesitter.start)
+
+                -- 3. Re-apply config to current window/buffer directly
+                local wo = vim.wo
+                local bo = vim.bo
+                wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                wo.foldmethod = "expr"
+                wo.foldlevel = 99
+                bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+                -- 4. Future buffers via FileType
                 vim.api.nvim_create_autocmd("FileType", {
                     callback = function()
                         local ok = pcall(vim.treesitter.start)
                         if not ok then return end
 
-                        local wo = vim.wo[0][0]
-                        wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                        wo.foldmethod = "expr"
-                        wo.foldlevel = 99
+                        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                        vim.wo.foldmethod = "expr"
+                        vim.wo.foldlevel = 99
                         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
                     end,
                 })
 
-                -- Kick treesitter on whatever buffer is already open
-                pcall(vim.treesitter.start)
-
-            end, 50) -- 50ms after VeryLazy
+            end, 50)
         end,
     },
 }
