2b3a3e364f94faa255dff54d788a544cde9a279e
anoninus <anoninuxt@gmail.com>
2026-06-08 09:57:08 +0530

    fixed issue where treesitter binary was not loading for available one

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -23,16 +23,16 @@ return {
                 })
                 :wait(300000)
 
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = { "lua", "javascript", "zig", "go", "python", "html", "css" },
-                callback = function()
-                    vim.treesitter.start()
-                    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                    vim.wo[0][0].foldmethod = "expr"
-                    vim.wo[0][0].foldlevel = 99 -- open all folds by default
-                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-                end,
-            })
+            -- vim.api.nvim_create_autocmd("FileType", {
+            --     pattern = { "lua", "javascript", "zig", "go", "python", "html", "css" },
+            --     callback = function()
+            vim.treesitter.start()
+            vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.wo[0][0].foldmethod = "expr"
+            vim.wo[0][0].foldlevel = 99 -- open all folds by default
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            -- end,
+            -- })
         end,
     },
 }
